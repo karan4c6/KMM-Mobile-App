485815e6a90f63eecff8711d7ab8d491fb113c19
Karan Sharma <karan.sharma@arq.group>
2021-01-18 12:33:58 +1100

    Implementing an API service

---
+++ shared/src/commonMain/kotlin/com/karansyd4/kmmapp/shared/network/SpaceXApi.kt
@@ -0,0 +1,28 @@
+package com.karansyd4.kmmapp.shared.network
+
+import com.karansyd4.kmmapp.shared.entity.RocketLaunch
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import kotlinx.serialization.json.Json
+
+/**
+ * This class will execute network requests and deserialize JSON responses into entities from the entity package.
+ */
+class SpaceXApi {
+    private val httpClient = HttpClient {
+        install(JsonFeature) {
+            val json = Json { ignoreUnknownKeys = true }
+            serializer = KotlinxSerializer(json)
+        }
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get(LAUNCHES_ENDPOINT)
+    }
+
+    companion object {
+        private const val LAUNCHES_ENDPOINT = "https://api.spacexdata.com/v3/launches"
+    }
+}
